c23ebfea762931fc392b90a5fba7f0556aafdd72
HAT Software Development Services <75292269+hatsoftware@users.noreply.github.com>
2022-05-31 12:35:37 -0700

    Add files via upload

--- navi.js
+++ navi.js
@@ -23,7 +23,7 @@ window.onpopstate = function (event) {
   //gotoDiv(content);
   //alert('window.onpopstate: '+content);
   if(content){ 
-    if(content=='home'){ gotoDiv_Home('wrapper'); }
+    if(content=='home'){ gotoDiv_Home('home'); }
     else if(content=='videos-all'){ showVideos(); }
     else if(content=='photos-all'){ showPhotos(); }
     else if(content=='articles-all'){ showArticles(); }
@@ -40,7 +40,7 @@ history.pushState( {
   plate_id: 1, 
   plate: 'home' 
 }, null, JBE_SITE);
-gotoDiv('wrapper');
+gotoDiv('home');
 
 //********************************************************** */
 
@@ -49,12 +49,13 @@ function gotoDiv_Page(d){
   gotoDiv(d);
 }
 
-function gotoDiv_Home(d){
+function gotoDiv_Home(){
   hist_push(1,"home",JBE_SITE);
-  gotoDiv(d);
+  gotoDiv('home');
 }
 
 function gotoDiv(d) {
+  //alert(d);
   var disp1='block';
   var disp2='none';
   if(d=='sap'){
@@ -65,11 +66,11 @@ function gotoDiv(d) {
     if(iframe) {
       //alert('found iframe');
       document.getElementById('sap').innerHTML='';
-      document.getElementById('wrapper').style.display='block';
+      document.getElementById('home').style.display='block';
     }
   }
   
-  document.getElementById('wrapper').style.display=disp1;
+  document.getElementById('home').style.display=disp1;
   document.getElementById('sap').style.display=disp2;
   var elmnt = document.getElementById(d);
   elmnt.classList.remove('w3-animate-opacity');
